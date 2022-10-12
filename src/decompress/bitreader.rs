02658a5ef77a7c08cfd37588b1871ceff8cec7a3
Nils Harbke <ekbrah@gmail.com>
2022-10-12 08:29:14 +0200

    Update

--- src/decompress/bitreader.rs
+++ src/decompress/bitreader.rs
@@ -19,11 +19,15 @@ impl<R: Read> BitReader<R> {
 
     /// Bit offset from the byte at the starting position of inner.
     pub fn with_offset(offset: u8, inner: R) -> BitReader<R> {
-        BitReader { 
-            inner, 
-            cached_bits_count: 8 - offset, 
-            cache: inner.read_u8().unwrap() << offset
-        }
+        assert!(offset < 8, "offset out of range!");
+        
+        let mut reader = BitReader::new(inner);
+
+        reader.read_u8(8);
+        reader.cached_bits_count = 8 - offset;
+        reader.cache = reader.cache << 8 + offset;
+
+        reader
     }
 
     pub fn read_u8(&mut self, count: u8) -> Option<u8> {
